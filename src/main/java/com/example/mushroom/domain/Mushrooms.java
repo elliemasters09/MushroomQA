e66023438a44211f4f6b14b6aca00d09d964f750
elliemasters09 <elliemasters09@gmail.com>
2022-02-08 20:02:21 +0000

    domain

--- src/main/java/com/example/mushroom/domain/Mushrooms.java
+++ src/main/java/com/example/mushroom/domain/Mushrooms.java
@@ -1,5 +1,23 @@
 package com.example.mushroom.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Mushrooms {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+		
+	private String latin;
+
+	private int deadliness;
+		
+	private String isEdible;
 
 }
